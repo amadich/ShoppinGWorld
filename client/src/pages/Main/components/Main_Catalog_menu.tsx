8d045138f37da35d5dff12390ba30972007e5b47
amadich <me@amadich.tn>
2024-10-19 22:16:20 +0100

    ShoppingWorld FIrst Push v:0.0

--- client/src/pages/Main/components/Main_Catalog_menu.tsx
+++ client/src/pages/Main/components/Main_Catalog_menu.tsx
@@ -2,39 +2,57 @@ import "../../../assets/styles/MainCatalog.css";
 
 export default function Main_Catalog_menu() {
   return (
-    <div className=" flex w-[80%] m-auto  mt-10 space-x-4 font-bold ">
-          <ul className="menu lg:menu-horizontal bg-[#dfdcdc] rounded-[50px] lg:mb-64 w-36">
-              
-              <li>
-                  <details>
-                    <summary id="ccb">Parent item</summary>
-                  </details>
-              </li>
-            </ul>
-            <ul className="menu lg:menu-horizontal bg-[#dfdcdc] rounded-[50px] lg:mb-64 w-36">
-              <li>
-                  <details>
-                    <summary id="ccb">Parent item</summary>
-                  </details>
-              </li>
-          </ul>
-          <ul className="menu lg:menu-horizontal bg-[#dfdcdc] rounded-[50px] lg:mb-64 w-36">
-              <li>
-                  <details>
-                    <summary id="ccb">Parent item</summary>
-                  </details>
-              </li>
-          </ul>
+    
+    <>
+        <div className="flex items-center justify-between w-[80%] mt-10 ">
+                      <div className=" flex items-center m-auto   space-x-4 font-bold ">
+                        
+                            <ul className="menu lg:menu-horizontal bg-[#dfdcdc] rounded-[50px] lg:mb-64 w-36">
+                                
+                                <li>
+                                    <details>
+                                      <summary id="ccb">Parent item</summary>
+                                    </details>
+                                </li>
+                              </ul>
+                              <ul className="menu lg:menu-horizontal bg-[#dfdcdc] rounded-[50px] lg:mb-64 w-36">
+                                <li>
+                                    <details>
+                                      <summary id="ccb">Parent item</summary>
+                                    </details>
+                                </li>
+                            </ul>
+                            <ul className="menu lg:menu-horizontal bg-[#dfdcdc] rounded-[50px] lg:mb-64 w-36">
+                                <li>
+                                    <details>
+                                      <summary id="ccb">Parent item</summary>
+                                    </details>
+                                </li>
+                            </ul>
 
-          <ul className="menu lg:menu-horizontal bg-[#dfdcdc] rounded-[50px] lg:mb-64 w-36">
+                            <ul className="menu lg:menu-horizontal bg-[#dfdcdc] rounded-[50px] lg:mb-64 w-36">
 
-              <li>
-                  <details>
-                    <summary id="ccb">Parent item</summary>
-                  </details>
-              </li>
+                                <li>
+                                    <details>
+                                      <summary id="ccb">Parent item</summary>
+                                    </details>
+                                </li>
 
-          </ul>
-    </div>
+                            </ul>
+                      </div>
+
+                        <div className="flex items-center">
+                            <ul className="menu lg:menu-horizontal border bg-transparent rounded-[50px] lg:mb-64 w-36">
+                                    
+                                    <li>
+                                        <details>
+                                          <summary id="ccb" className="text-center font-bold">Sert By</summary>
+                                        </details>
+                                    </li>
+                            </ul>
+                        </div>
+        </div>
+    </>
+
   )
 }

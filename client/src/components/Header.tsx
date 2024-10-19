c903c7a5fd8bb546b27e182011fcfced9f3b5574
amadich <me@amadich.tn>
2024-10-19 20:03:25 +0100

    ShoppingWorld FIrst Push v:0.0

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -13,8 +13,57 @@ export default function Header() {
       </div>
       
       <nav>
-        <ul className=" flex items-center gap-4 " id="headermain_ul">
-          <li> Categories </li>
+        <ul className=" flex items-center gap-4 menu lg:menu-horizontal rounded-box " id="headermain_ul">
+        <li>
+          <details open>
+            <summary id="cca">Category</summary>
+            <ul className="menu xl:menu-horizontal rounded-box lg:min-w-max bg-white" >
+                  <li>
+                    <a>Solutions</a>
+                    <ul>
+                      <li><a>Design</a></li>
+                      <li><a>Development</a></li>
+                      <li><a>Hosting</a></li>
+                      <li><a>Domain register</a></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a>Enterprise</a>
+                    <ul>
+                      <li><a>CRM software</a></li>
+                      <li><a>Marketing management</a></li>
+                      <li><a>Security</a></li>
+                      <li><a>Consulting</a></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a>Products</a>
+                    <ul>
+                      <li><a>UI Kit</a></li>
+                      <li><a>Wordpress themes</a></li>
+                      <li><a>Wordpress plugins</a></li>
+                      <li>
+                        <a>Open source</a>
+                        <ul>
+                          <li><a>Auth management system</a></li>
+                          <li><a>VScode theme</a></li>
+                          <li><a>Color picker app</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a>Company</a>
+                    <ul>
+                      <li><a>About us</a></li>
+                      <li><a>Contact us</a></li>
+                      <li><a>Privacy policy</a></li>
+                      <li><a>Press kit</a></li>
+                    </ul>
+                  </li>
+                </ul>
+          </details>
+        </li>
           <li> Products </li>
           <li> What's News </li>
           <li>Delivery</li>

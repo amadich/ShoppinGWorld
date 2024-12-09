b06b7df45dc527e6fedd972a671e48ec070b9321
amadich <me@amadich.tn>
2024-12-10 00:34:49 +0100

    ShoppingWorld v:1.0.5

--- client/src/firebase.ts
+++ client/src/firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,4 +20,3 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
-const analytics = getAnalytics(app);

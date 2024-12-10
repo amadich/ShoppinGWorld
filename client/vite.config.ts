8924520d735872c1146e1e192290c4df24938cd0
amadich <me@amadich.tn>
2024-12-10 01:24:48 +0100

    Fix: Update vercel.json and Vite config for deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist' // This ensures Vercel looks for the dist directory for your built assets
-  }
 })
